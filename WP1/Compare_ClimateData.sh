284b934cc26957df06cfdb167cc99e10d038bb99
ninsbl <stefan.blumentrath@gmx.de>
2016-11-09 14:26:23 +0100

    plot differences in climate data

--- WP1/Compare_ClimateData.sh
+++ WP1/Compare_ClimateData.sh
@@ -37,7 +37,50 @@ done
 # Calculate differences between EuroLST and SeNorge for bioclim variables
 for b in 01 02 03 04 05 06 07 10 11
 do
-# Compute within-pixel variation between 250m EuroLST and 1km SeNorge data
+# Compute difference between 250m EuroLST aggregated to 1km and 1km SeNorge data
 r.mapcalc --o --v expression="eurolst_SeNorge_bio${b}_1km_difference=eurolst_clim.bio${b}_1km_mean - temperature_seNorge_1km_bioclim_bio${b}_2001_2010_mean"
+# Compute differen1km SeNorge data and 1km WorldClim
+#r.mapcalc --o --v expression="SeNorge_WorldClim_bio${b}_1km_difference=temperature_seNorge_1km_bioclim_bio${b}_2001_2010_mean - WorldClim_current_bio05_1975@g_Meteorology_Fenoscandia_WorldClim_current"
+
+# Set color table to "differences"
+r.colors map=eurolst_SeNorge_bio${b}_1km_difference color=differences
+done
+
+for b in 01 02 03 04 05 06 07 10 11
+do
+# Get title of bioclim variable
+if [ "bio${b}" == "bio01" ] ; then 
+var="Annual Mean Temperature"
+elif [ "bio${b}" == "bio02" ] ; then 
+var="Mean Diurnal Range (Mean of monthly (max temp - min temp))"
+elif [ "bio${b}" == "bio03" ] ; then 
+var="Isothermality (BIO2/BIO7) (* 100)"
+elif [ "bio${b}" == "bio04" ] ; then 
+var="Temperature Seasonality (standard deviation *100)"
+elif [ "bio${b}" == "bio05" ] ; then 
+var="Max Temperature of Warmest Month"
+elif [ "bio${b}" == "bio06" ] ; then 
+var="Min Temperature of Coldest Month"
+elif [ "bio${b}" == "bio07" ] ; then 
+var="Temperature Annual Range (BIO5-BIO6)"
+elif [ "bio${b}" == "bio08" ] ; then 
+var="Mean Temperature of Wettest Quarter"
+elif [ "bio${b}" == "bio09" ] ; then 
+var="Mean Temperature of Driest Quarter"
+elif [ "bio${b}" == "bio10" ] ; then 
+var="Mean Temperature of Warmest Quarter"
+elif [ "bio${b}" == "bio11" ] ; then 
+var="Mean Temperature of Coldest Quarter"
+fi
+
+# Make a simple plot for each variable
+d.mon --o start=cairo output=$HOME/Prosjekter/Climate\ Ecotones/WP1/ClimateDataComparison/eurolst_SeNorge_bio${b}_1km_difference.png
+d.rast map=eurolst_SeNorge_bio${b}_1km_difference
+d.text -p -s text="Difference between EuroLST (E) and SeNorge (S) (E-S)" color=black linespacing=1 at=5,25 font=arialbd size=12
+d.text -p -s text="BIO${b}: $var" color=black linespacing=1 at=5,45 font=arialbd size=12
+d.legend -d raster=eurolst_SeNorge_bio${b}_1km_difference title="Temperature difference" title_fontsize=14 lines=1 units=" C/10" at=5,50,10,12.5 font=Arial fontsize=12
+d.barscale at=50.0,10.0 length=500 units=kilometers segment=9
+d.northarrow at=85.0,15.0 fontsize=12
+d.mon stop=cairo
 done
 
