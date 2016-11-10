fb8a60cfcf3710cbc60a119ad584eb85fb559ffb
ninsbl <stefan.blumentrath@gmx.de>
2016-11-10 11:17:20 +0100

    climatedata comparison coordinates

--- WP1/metno_stations.sql
+++ WP1/metno_stations.sql
@@ -1,2 +1,2 @@
-SELECT * FROM (SELECT DISTINCT ON (geom) stnr, ST_X(ST_Transform(geom, 3035)) AS X, ST_Y(ST_Transform(geom, 3035)) AS Y
+SELECT * FROM (SELECT DISTINCT ON (geom) ST_X(ST_Transform(geom, 3035)) AS X, ST_Y(ST_Transform(geom, 3035)) AS Y, stnr
   FROM "Meteorology"."metnoStations") AS x ORDER BY stnr;
--- WP1/templogger_locations.sql
+++ WP1/templogger_locations.sql
@@ -1,2 +1,8 @@
-SELECT * FROM (SELECT DISTINCT ON (geom) gid, ST_X(ST_Transform(geom, 3035)) AS X, ST_Y(ST_Transform(geom, 3035)) AS Y
-  FROM sentinel4nature."temperaturelogger_locations_Nina_Eide") AS x ORDER BY gid;
+SELECT * FROM (SELECT DISTINCT ON (geom) ST_X(ST_Transform(geom, 3035)) AS X, ST_Y(ST_Transform(geom, 3035)) AS Y, CAST('temp_' || gid AS varchar(15)) AS id
+  FROM sentinel4nature."temperaturelogger_locations_Nina_Eide") AS x
+UNION ALL 
+SELECT * FROM (SELECT DISTINCT ON (geom) ST_X(ST_Transform(geom, 3035)) AS X, ST_Y(ST_Transform(geom, 3035)) AS Y, CAST('ce_gps_' || gid AS varchar(15)) AS id
+  FROM climateecotones.ce_gps_waypoints_2016) AS y
+UNION ALL 
+SELECT * FROM (SELECT DISTINCT ON (geom) ST_X(ST_Transform(geom, 3035)) AS X, ST_Y(ST_Transform(geom, 3035)) AS Y, CAST('hjerk_gps_' || gid AS varchar(15)) AS id
+  FROM climateecotones.hjerkin_sip_gps_waypoints_2016) AS z ORDER BY id;
